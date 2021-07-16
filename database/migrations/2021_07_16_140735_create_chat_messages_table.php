e579c286049064b911e41f8015356e949b732cf9
Diogo Antunes <diogololz159@gmail.com>
2021-07-16 11:19:30 -0300

    Added ChatRoom and ChatMessage models

---
+++ database/migrations/2021_07_16_140735_create_chat_messages_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\ChatRoom;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChatMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chat_messages', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->foreignIdFor(ChatRoom::class);
+            $table->foreignIdFor(User::class);
+
+            $table->mediumText('message');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chat_messages');
+    }
+}
